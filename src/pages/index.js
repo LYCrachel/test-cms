83bdc8fc38d14cc48884d31e1dc0b2ff5256db40
masakiNagaoka <masaking040507@gmail.com>
2018-02-20 18:45:03 +0900

    mainのみへの変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,9 +28,9 @@ export default class IndexPage extends React.Component {
 
     return (
       <div>
-        <section className="hero is-fullheight">
+        <div className="hero is-fullheight">
           <div className="hero-body yuba">
-            <div className="hero-body yuba2 is-fullheight">
+            <div className="hero-body yuba2">
               <div className="container has-text-centered">
                 <figure className="image-masako-logo">
                   <img src={logo} alt="logo" width="200" />
@@ -44,105 +44,103 @@ export default class IndexPage extends React.Component {
               </div>
             </div>
           </div>
-        </section>
+        </div>
         <Script
           url="https://identity.netlify.com/v1/netlify-identity-widget.js"
           onLoad={() => this.handleScriptLoad()}
         />
-        <div className="container is-flex-desktop main">
-          <div className="main_content">
-            <div className="information">
-              {posts
-                .filter(
-                  post => post.node.frontmatter.templateKey === "blog-post"
-                )
-                .map(({ node: post }) => (
-                  <div className="information-contents" key={post.id}>
-                    <Link
-                      className="information-item"
-                      to={post.frontmatter.path}
-                    >
-                      {post.frontmatter.title}
-                      <small className="information-date">
-                        {post.frontmatter.date}
-                      </small>
-                    </Link>
-                  </div>
-                ))}
-            </div>
+        <div className="container">
+          <section className="information">
+            {posts
+              .filter(
+                post => post.node.frontmatter.templateKey === "blog-post"
+              )
+              .map(({ node: post }) => (
+                <div className="information-contents" key={post.id}>
+                  <Link
+                    className="information-item"
+                    to={post.frontmatter.path}
+                  >
+                    {post.frontmatter.title}
+                    <small className="information-date">
+                      {post.frontmatter.date}
+                    </small>
+                  </Link>
+                </div>
+              ))}
+          </section>
 
-            <div className="greeting">
-              <div className="greeting-head">
-                <p className="greeting-1">ご訪問ありがとうございます</p>
-                <p className="greeting-2">
-                  今年1月に南手城町にリニューアルオープンしました
-                </p>
-                <p className="greeting-3">
-                  美しさと健康に導く「手」のある癒しの空間
-                </p>
-                <h2 className="greeting-4">
-                  <span>レ・マンズ・M (エム)</span>です。
-                </h2>
+          <section className="greeting">
+            <div className="greeting-head">
+              <p className="greeting-1">ご訪問ありがとうございます</p>
+              <p className="greeting-2">
+                今年1月に南手城町にリニューアルオープンしました
+              </p>
+              <p className="greeting-3">
+                美しさと健康に導く「手」のある癒しの空間
+              </p>
+              <h2 className="greeting-4">
+                <span>レ・マンズ・M (エム)</span>です。
+              </h2>
+            </div>
+            <div className="greeting-middle">
+              <div className="space">
+                <figure className="space-img">
+                  <img
+                    src={counseling}
+                    alt="counseling"
+                    style={{ width: "200px" }}
+                  />
+                </figure>
               </div>
-              <div className="greeting-middle">
-                <div className="space">
-                  <figure className="space-img">
-                    <img
-                      src={counseling}
-                      alt="counseling"
-                      style={{ width: "200px" }}
-                    />
-                  </figure>
-                </div>
-                <div className="text-area">
-                  <p className="text-top">サロンスペースでは、</p>
-                  <p className="text-contents">
-                    お客様へのカウンセリングを行ったり<br />
-                    施術後にオススメのお茶で寛いで頂いており
-                  </p>
-                  <p className="text-contents">
-                    <span>「ゆったりできる」</span>と評判です。
-                  </p>
-                </div>
-              </div>
-              <div className="greeting-bottom">
-                <div className="text-area2">
-                  <p className="text2-top">「ついつい寝てしまいます」</p>
-                  <p className="text2-contents">
-                    と、お声を頂く施術スペースでは<br />
-                    お一人お一人の体質・体調に合わせた<br />
-                    <span>レ・マンズ・M (エム)オリジナル</span>
-                    <br />
-                    <span>トリートメントメソッド</span>で<br />
-                    フェイシャル、ヘッドスパ<br />
-                    そして全身にわたりリンパマッサージを行い
-                  </p>
-                  <p className="text2-contents">
-                    お客様の美と健康に磨きをかけるお手伝いを<br />
-                    させていただいています。
-                  </p>
-                </div>
-                <div className="space">
-                  <figure className="space-img">
-                    <img src={space} alt="space" style={{ width: "200px" }} />
-                  </figure>
-                </div>
+              <div className="text-area">
+                <p className="text-top">サロンスペースでは、</p>
+                <p className="text-contents">
+                  お客様へのカウンセリングを行ったり<br />
+                  施術後にオススメのお茶で寛いで頂いており
+                </p>
+                <p className="text-contents">
+                  <span>「ゆったりできる」</span>と評判です。
+                </p>
               </div>
             </div>
-          </div>
-          <div className="sidebar">
-            <div className="masako">
-              <div className="is-flex-desktop">
-                <figure className="image-masako">
-                  <img src={masako} alt="masako" style={{ width: "200px" }} />
-                </figure>
-                <div className="name">
-                  <p className="is-size-6 has-text-centered">長岡 末佐子</p>
-                  <p className="is-size-7 has-text-centered">(ながおか まさこ)</p>
-                </div>
+            <div className="greeting-bottom">
+              <div className="text-area2">
+                <p className="text2-top">「ついつい寝てしまいます」</p>
+                <p className="text2-contents">
+                  と、お声を頂く施術スペースでは<br />
+                  お一人お一人の体質・体調に合わせた<br />
+                  <span>レ・マンズ・M (エム)オリジナル</span>
+                  <br />
+                  <span>トリートメントメソッド</span>で<br />
+                  フェイシャル、ヘッドスパ<br />
+                  そして全身にわたりリンパマッサージを行い
+                </p>
+                <p className="text2-contents">
+                  お客様の美と健康に磨きをかけるお手伝いを<br />
+                  させていただいています。
+                </p>
               </div>
+              <div className="space">
+                <figure className="space-img">
+                  <img src={space} alt="space" style={{ width: "200px" }} />
+                </figure>
+              </div>
+            </div>
+          </section>
+
+          <section className="introduction">
+            <h1 className="has-text-centered is-size-3">INTRODUCTION</h1>
+            <div className="masako  is-flex-desktop">
+              <figure className="image-masako">
+                <img src={masako} alt="masako" style={{ width: "200px" }} />
+              </figure>
               <div className="masako-info">
-                <p className="masako-text">
+                <div className="name">
+                  <p className="is-size-3 has-text-centered">長岡 末佐子</p>
+                  <p className="is-size-4 has-text-centered">(ながおか まさこ)</p>
+                </div>
+                <p className="masako-text has-text-centered is-size-5">
                   レ・マンズ・Ｍ技術認定スクール代表<br />
                   (社)日本予防医学アカデミー認定校<br />
                   予防医学健康美協会リンパケアマネジャー<br />
@@ -151,39 +149,35 @@ export default class IndexPage extends React.Component {
                 </p>
               </div>
             </div>
-            {/* <div className="accessMap">
+          </section>
+
+          <section className="tel">
+            <p className="tel-text is-size-4 has-text-centered">お問い合わせはこちら</p>
+            <p className="tel-number is-size-3 has-text-centered">090-8245-3516</p>
+          </section>
+
+
+          <section className="access continer">
+            <h1 className="has-text-centered is-size-2">ACCESS</h1>
+            <div className="adress">
+              <p>〒 721-0963</p>
+              <p>広島県福山市南手城町3丁目7番9号</p>
+            </div>
+            <div>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.7487104581096!2d133.39406931462378!3d34.483898502307376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35511142c48dc3d5%3A0x330fb0bb6803506!2z44CSNzIxLTA5NjMg5bqD5bO255yM56aP5bGx5biC5Y2X5omL5Z-O55S677yT5LiB55uu77yX4oiS77yZ!5e0!3m2!1sja!2sjp!4v1518140441835"
                 style={{
                   border: "0",
                   width: "100%",
-                  height: "200",
+                  height: "500px",
                   frameborder: "0"
                 }}
               />
-              <div className="address">
-                <p className="addressNumber">〒 721-0963</p>
-                <p className="address-text">広島県福山市南手城町3丁目7番9号</p>
-              </div>
-            </div> */}
-            <div className="tel">
-              <p className="tel-text is-size-4 has-text-centered">お問い合わせはこちら</p>
-              <p className="tel-number is-size-3 has-text-centered">090-8245-3516</p>
             </div>
-          </div>
+          </section>
         </div>
-        <footer className="container is-semimiddle">
-          <div>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.7487104581096!2d133.39406931462378!3d34.483898502307376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35511142c48dc3d5%3A0x330fb0bb6803506!2z44CSNzIxLTA5NjMg5bqD5bO255yM56aP5bGx5biC5Y2X5omL5Z-O55S677yT5LiB55uu77yX4oiS77yZ!5e0!3m2!1sja!2sjp!4v1518140441835"
-              style={{
-                border: "0",
-                width: "100%",
-                height: "100vh",
-                frameborder: "0"
-              }}
-            />
-          </div>
+        <footer className="footer">
+
         </footer>
       </div>
     );
