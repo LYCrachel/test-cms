aa52a3ffbb49e1eab49b28b93915b85671ddcb04
masakiNagaoka <masaking040507@gmail.com>
2018-03-28 17:30:43 +0900

    もろもろ変更

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import title from "../img/レマンズ.gif";
 
 
 const Navbar = () => (
-  <nav className="navbar is-fixed-top is-flex is-transparent nav-height">
+  <nav className="navbar is-fixed-top is-flex is-transparent nav-height" id="nav">
     <div className="navbar-brand">
       <Link to="/" className="navbar-item">
         <img className="navbar-logo" src={logo} alt="logo" width="48" />
@@ -17,12 +17,31 @@ const Navbar = () => (
     <div className="navbar-start" />
 
     <div className="navbar-end is-flex contents-height">
-      <Link className="navbar-item" to="/about">
+      <Link className="navbar-item" to="/#information">
+        <span className="navbar-title">INFORMATION</span>
+      </Link>
+      <Link className="navbar-item" to="/#salon">
+        <span className="navbar-title">SALON CONCEPT</span>
+      </Link>
+      <Link className="navbar-item" to="/#introduction">
+        <span className="navbar-title">INTRODUCTION</span>
+      </Link>
+      <Link className="navbar-item" to="/#school_c">
+        <span className="navbar-title">SCHOOL CONCEPT</span>
+      </Link>
+      <Link className="navbar-item" to="/#school">
+        <span className="navbar-title">SCHOOL</span>
+      </Link>
+      <Link className="navbar-item" to="/#menu">
         <span className="navbar-title">MENU</span>
       </Link>
-      <Link className="navbar-item" to="/products">
+      <Link className="navbar-item" to="/#contact">
+        <span className="navbar-title">CONTACT</span>
+      </Link>
+      <Link className="navbar-item" to="/#access">
         <span className="navbar-title">ACCESS</span>
       </Link>
+
     </div>
   </nav>
 );
